c8a130d1d4a1d63e880458deac24bd1cf8fa6986
LoboAnimae <andyquan12@gmail.com>
2020-07-10 22:57:47 -0600

    First commit

---
+++ operations.py
@@ -0,0 +1,24 @@
+def glinit():
+    pass
+
+def gl-create-window():
+    pass
+
+def gl-view-port():
+    pass
+
+def gl-clear():
+    pass
+
+def gl-clear-color():
+    pass
+
+def gl-vertex():
+    pass
+
+def gl-color():
+    pass
+
+def gl-finish():
+    pass
+
